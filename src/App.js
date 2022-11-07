507804816f336337ab19ac532e57140845948995
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-11-07 18:37:52 -0500

    added Bootstrap as a dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Home from "./pages/Home"
 
 function App() {
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      {/* TODO: Add logo here */}
+      <nav className="text-center">
+        {/* TODO: Add Buttons to navigate to the other pages */}
+        <h2>Link</h2>
+        <h2>Link</h2>
+        <h2>Link</h2>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
