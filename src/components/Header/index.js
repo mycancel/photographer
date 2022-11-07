9bb68c08fbcb36b452438b49abdb0ea56d790ab9
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-11-07 18:59:15 -0500

    added bootstrap styling to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
+    <header style={{backgroundColor: "grey"}} className="pt-3 pb-3">
       {/* TODO: Add logo here */}
-      <nav className="text-center">
+      <nav className="text-center d-flex p-2 justify-content-around">
         {/* TODO: Add Buttons to navigate to the other pages */}
-        <h2>Link</h2>
-        <h2>Link</h2>
-        <h2>Link</h2>
+        <h2 className="m-2">Link</h2>
+        <h2 className="m-2">Link</h2>
+        <h2 className="m-2">Link</h2>
       </nav>
     </header>
   )
