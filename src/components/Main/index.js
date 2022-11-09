b6b2b19ee1cfe5f998721f87111cafffcedf9810
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-11-08 23:06:28 -0500

    added placeholder content to main component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Main = () => {
   // TODO: Add content to Homepage
   return (
-    <main>Main</main>
+    <main>
+      <h3 className='text-center'>Fancy Quote Here <span>- Abraham Lincoln</span></h3>
+      <section className='container'>
+      <h2>See the Latest</h2>
+        <div className='row'>
+          <img className='col-12 col-md-6 col-lg-4 mt-4' alt='samplePhoto2' src='/images/placeholder.png'></img>
+          <img className='col-12 col-md-6 col-lg-4 mt-4' alt='samplePhoto3' src='/images/placeholder.png'></img>
+          <img className='col-12 col-md-6 col-lg-4 mt-4' alt='samplePhoto1' src='/images/placeholder.png'></img>
+          <img className='col-12 col-md-6 col-lg-4 mt-4' alt='samplePhoto1' src='/images/placeholder.png'></img>
+          <img className='col-12 col-md-6 col-lg-4 mt-4' alt='samplePhoto1' src='/images/placeholder.png'></img>
+          <img className='col-12 col-md-6 col-lg-4 mt-4' alt='samplePhoto1' src='/images/placeholder.png'></img>
+        </div>
+      </section>
+    </main>
   )
 }
 
