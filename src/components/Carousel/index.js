889ac58b20a8660fa8dc2ea3d8ca7eb6ef56b7e1
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-11-08 22:14:47 -0500

    changed class to className in Carousel component; added comment; changed div to section in Carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Carousel = () => {
   // TODO: Fix Carousel so pictures switch
+  // https://getbootstrap.com/docs/4.0/components/carousel/
 
   return (
-    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+    <section id="carouselExampleControls" className="carousel slide" data-ride="carousel">
 
       <div className="carousel-inner">
         <div className="carousel-item active">
@@ -18,15 +19,15 @@ const Carousel = () => {
         </div>
       </div>
 
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+      <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
       </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
+      <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
       </a>
-    </div>
+    </section>
   )
 }
 
